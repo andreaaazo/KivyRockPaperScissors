43b20cdf0dff7dbf9a79f49abd4001a6eb9cf9dc
andreaaazo <zorzi.andrea@outlook.com>
2022-07-30 20:58:43 +0200

    Added basic GUI

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MainWidget(BoxLayout):
+    pass
+
+
+class RockPaperScissorsApp(App):
+    pass
+
+
+RockPaperScissorsApp().run()
