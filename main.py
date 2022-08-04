374b81ea5dcf016d29011061f5b6a7fb78d551d0
andreaaazo <zorzi.andrea@outlook.com>
2022-08-04 20:05:04 +0300

    Added ai choosing animation, added check if player wins

--- main.py
+++ main.py
@@ -1,100 +1,139 @@
-from multiprocessing import current_process
+from concurrent.futures import thread
 from secrets import choice
 from kivy.app import App
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.properties import StringProperty, ObjectProperty
 from kivy.clock import Clock
 import kivy.utils as utils
 from time import sleep
 import threading
-from random import randint
+from random import randint, choice
+
+from matplotlib import use
 
 
 class MainWidget(RelativeLayout):
-    user_choices = {
+    choices = {
         "rock": "lib/rock.png",
         "scissors": "lib/scissors.png",
         "paper": "lib/paper.png",
     }
+
+    ai_choice = None
+    user_choice = None
+
+    user_choice_img = None
+    ai_choice_img = None
+
+    scoreboard_text = None
+    scoreboard = None
+
+    ai_score = 0
+    user_score = 0
+
+    main_thread = threading.Thread
+    thread_ai_animation = None
+    thread_check_win = None
+
+    main_color = "#84A98C"
+    second_color = "#2F3E46"
+    third_color = "#CAD2C5"
 
     def __init__(self, **kw):
         super().__init__(**kw)
         Clock.schedule_once(self.init_variables, 0)
 
     def init_variables(self, *args):
-        self.user_choice = self.ids.user_choice
-        self.ai_choice_image = self.ids.ai_choice_image
+        self.user_choice_img = self.ids.user_choice_img
+        self.ai_choice_img = self.ids.ai_choice_img
+        self.scoreboard = self.ids.scoreboard
 
     def on_press_rock_button(self):
-        choice = self.user_choices["rock"]
-        self.user_choice.source = choice
-        self.ai_choice()
-        self.check_win(self.ai_choice_text, choice)
+        self.user_choice = "rock"
+        self.main_thread(target=self.main).start()
 
     def on_press_scissors_button(self):
-        choice = self.user_choices["scissors"]
-        self.user_choice.source = choice
-        self.ai_choice()
-        self.check_win(self.ai_choice_text, choice)
+        self.user_choice = "scissors"
+        self.main_thread(target=self.main).start()
 
     def on_press_paper_button(self):
-        choice = self.user_choices["paper"]
-        self.user_choice.source = choice
-        self.ai_choice()
-        self.check_win(self.ai_choice_text, choice)
+        self.user_choice = "paper"
+        self.main_thread(target=self.main).start()
+
+    def main(self):
+        self.change_user_choice_img()
+        self.ai_turn()
+        self.check_win()
 
     def set_button_color(self, button):
-        selected_button = button.canvas.before.get_group("color")[0]
-        selected_button.rgb = utils.get_color_from_hex("#84a98c")
+        button_bg = button.canvas.before.get_group("color")[0]
+
+        # Change button background
+        button_bg.rgb = utils.get_color_from_hex(self.main_color)
 
     def reset_button_color(self, button):
-        selected_button = button.canvas.before.get_group("color")[0]
-        selected_button.rgb = utils.get_color_from_hex("#2f3e46")
+        button_bg = button.canvas.before.get_group("color")[0]
 
-    def ai_choice(self, *args):
-        random_choice = randint(0, 2)
-        random_sleep = randint(5, 10)
+        # Change button background
+        button_bg.rgb = utils.get_color_from_hex(self.second_color)
 
-        val = list(self.user_choices.values())
-        self.ai_choice_text = val[random_choice]
+    def ai_turn(self, *args):
+        self.ai_choice = choice(list(self.choices.keys()))
 
-        def animate():
-            for _ in range(0, random_sleep):
-                for i in self.user_choices.values():
-                    self.ai_choice_image.source = i
+        ai_choice_img = self.choices[self.ai_choice]
+        duration_anim = randint(5, 10)
+
+        def animations():
+            for _ in range(0, duration_anim):
+                for i in self.choices:
+                    image = self.choices[i]
+                    self.ai_choice_img.source = image
                     sleep(0.1)
-            self.ai_choice_image.source = self.ai_choice_text
+            self.ai_choice_img.source = ai_choice_img
 
-        threading.Thread(target=animate).start()
-        return self.ai_choice_text
+        self.thread_ai_animation = threading.Thread(target=animations)
+        self.thread_ai_animation.start()
 
-    def check_win(self, ai_choice, user_choice):
-        rock = self.user_choices["rock"]
-        scissors = self.user_choices["scissors"]
-        paper = self.user_choices["paper"]
-        if ai_choice == rock:
-            if user_choice == paper:
-                print("WIN")
-            elif user_choice == rock:
-                print("TIE")
+    def check_win(self):
+        ai_choice = self.ai_choice
+        user_choice = self.user_choice
+
+        def start():
+            self.thread_ai_animation.join()
+
+            if user_choice == "rock":
+                if ai_choice == "scissors":
+                    self.scoreboard_text = "WIN"
+                elif ai_choice == "rock":
+                    self.scoreboard_text = "TIE"
+                else:
+                    self.scoreboard_text = "LOSE"
+
+            elif user_choice == "scissors":
+                if ai_choice == "paper":
+                    self.scoreboard_text = "WIN"
+                elif ai_choice == "scissors":
+                    self.scoreboard_text = "TIE"
+                else:
+                    self.scoreboard_text = "LOSE"
+
             else:
-                print("LOSE")
+                if ai_choice == "rock":
+                    self.scoreboard_text = "WIN"
+                elif ai_choice == "paper":
+                    self.scoreboard_text = "TIE"
+                else:
+                    self.scoreboard_text = "LOSE"
 
-        if ai_choice == scissors:
-            if user_choice == rock:
-                print("WIN")
-            elif user_choice == scissors:
-                print("TIE")
-            else:
-                print("LOSE")
+        self.thread_check_win = threading.Thread(target=start)
+        self.thread_check_win.start()
 
-        if ai_choice == paper:
-            if user_choice == scissors:
-                print("WIN")
-            elif user_choice == paper:
-                print("TIE")
-            else:
-                print("LOSE")
+    def change_user_choice_img(self):
+        self.user_choice_img.source = self.choices[self.user_choice]
+
+    def update_result_to_gui(self):
+        scoreboard = self.scoreboard
+
+        scoreboard.text = self.scoreboard_text
 
 
 class RockPaperScissorsApp(App):
